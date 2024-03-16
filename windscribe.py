07a4eeb85d044cee340dc91782687b5a7dee63e4
Biskweet <gaspard.dama@gmail.com>
2024-03-16 13:39:45 +0100

    Not working yet

--- windscribe.py
+++ windscribe.py
@@ -1,15 +1,36 @@
-import requests as r
+import requests
 import json
+import dotenv
+import os
+
+dotenv.load_dotenv(".env")
 
 
 HEADERS = {
 	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0"
 }
 
+
+s = requests.Session()
+s.headers.update(HEADERS)
+
 # Fake website request
-login_req = r.get("https://windscribe.com/login", headers=HEADERS)
+login_req = s.get("https://windscribe.com/login")
 
 # Now let's obtain the CSRF token
-csrf_req = r.post("https://res.windscribe.com/res/logintoken", headers=HEADERS)
+csrf_req = s.post("https://res.windscribe.com/res/logintoken")
 csrf_data = json.loads(csrf_req.text)
+
+# looks like
+# {'csrf_token': '8580b1028025b7c0d1864be349a3d5a975818476', 'csrf_time': 1710589909}
+
+payload = {
+    "login": 1,
+    "upgrade": 0,
+    "csrf_time": csrf_data["csrf_time"],
+    "csrf_token": csrf_data["csrf_token"],
+    "username": os.environ["WINDSCRIBE_USERNAME"],
+    "password": os.environ["WINDSCRIBE_PASSWORD"],
+    "code": ''
+}
 
